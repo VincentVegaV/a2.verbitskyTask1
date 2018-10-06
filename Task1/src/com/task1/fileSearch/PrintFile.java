c9816363e81d0cfe1e14bf19919b68a35fec4047
VincentVegaV <forgitmail@gmail.com>
2018-10-06 15:30:46 +0300

    Minor changes

--- Task1/src/com/task1/fileSearch/PrintFile.java
+++ Task1/src/com/task1/fileSearch/PrintFile.java
@@ -2,23 +2,29 @@ package com.task1.fileSearch;
 
 import java.nio.file.attribute.FileTime;
 import java.time.ZoneId;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
  class PrintFile {
+     private static int fileCount;
+
      static void printFiles(Map<String, FileTime> list){
+
         ResourceBundle resourceBundleConfig = ResourceBundle.getBundle("Resources.Config");
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(
+                 "Resources.Messages", Locale.getDefault());
 
         Map.Entry<String, FileTime> newestFile = list.entrySet().iterator().next();
-
         long value = newestFile.getValue().toMillis() - Long.parseLong(resourceBundleConfig.getString("timeDifference"));
 
         for (Map.Entry<String, FileTime> mapEntry : list.entrySet()){
             if(mapEntry.getValue().toMillis() >= value){
-                System.out.println(mapEntry.getKey() + ": \n" + mapEntry.getValue().toInstant().atZone(ZoneId.systemDefault()));
+                System.out.println(mapEntry.getKey() + ": \n" + mapEntry.getValue().toInstant().atZone(ZoneId.systemDefault()) + resourceBundle.getString("newLine"));
+                ++fileCount;
             }
-
         }
+        System.out.println(resourceBundle.getString("found") + fileCount);
     }
 
 }
--- Task1/src/com/task1/fileSearch/Runner.java
+++ Task1/src/com/task1/fileSearch/Runner.java
@@ -1,18 +1,13 @@
 package com.task1.fileSearch;
 
 import java.io.IOException;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 class Runner {
+
     public static void main(String[] args){
-        SearchFile search = new SearchFile("E:\\test", "bmp");
+        SearchFile search = new SearchFile(args[0], args[1]);
 
-        ResourceBundle resourceBundle = ResourceBundle.getBundle(
-                "Resources.Messages", Locale.getDefault());
         try{
-            System.out.println(resourceBundle.getString("found"));
-
             PrintFile.printFiles(SortFile.sortListOfFilesDESC(search.returnListOfFileNameAndDate()));
         }catch (IOException e) {
             e.printStackTrace();
