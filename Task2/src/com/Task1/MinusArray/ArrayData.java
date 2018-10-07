647bf229360381278bd3dddc6c44a50c70aa91ed
VincentVegaV <forgitmail@gmail.com>
2018-10-07 13:25:50 +0300

    Task 2: Middle array added

--- Task2/src/com/Task1/MinusArray/ArrayData.java
+++ Task2/src/com/Task1/MinusArray/ArrayData.java
@@ -1,15 +1,8 @@
 package com.Task1.MinusArray;
 
-/**
- * "ArrayData" class contain array fields
- *
- * @author a2.verbitsky
- * @version 1.0
- */
 class ArrayData {
-    /**Field minuend*/
     String[] array1 = {"Alex", "Dima", "Kate", "Galina", "Ivan"};
-    /**Field subtrahend*/
-    String[] array2 = {"Dima", "Ivan", "Kate"};
+
+    String[] array2 = {"Dima", "Ivan", "Kate", "Galina", "Ivan"};
 
 }
--- Task2/src/com/Task1/MinusArray/ArraySubtraction.java
+++ Task2/src/com/Task1/MinusArray/ArraySubtraction.java
@@ -3,36 +3,19 @@ package com.Task1.MinusArray;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-/**
- * "ArraySubtraction" class contain methods that can subtract
- * two string arrays in different ways: using arrays or collections.
- *
- * @author a2.verbitsky
- * @version 1.0
- */
 class ArraySubtraction
 {
 
-    /**
-     * Method subtract array2 from array1 (using indexes) and returns result array
-     *
-     * @param array1 string type array
-     * @param array2 string type array
-     * @return String type result array (array1 - array2)
-     */
     String[] getSubtractedArray(String[] array1, String[] array2)
     {
-        int len1 = array1.length;
-        int len2 = array2.length;
         int resL = 0;
-        int n = len1-len2;
-        String[] res = new String[n];
+        String[] res = new String[array1.length];
         boolean isFound;
 
-        for(int i=0; i<=len1-1; i++)
+        for(int i=0; i<=array1.length-1; i++)
         {
             isFound = false;
-            for(int j=0; j<=len2-1; j++)
+            for(int j=0; j<=array2.length-1; j++)
             {
                 if(array1[i].equals(array2[j]))
                 {
@@ -45,16 +28,13 @@ class ArraySubtraction
                 resL++;
             }
         }
-        return res;
+        String[] dest_arr = new String[resL];
+        System.arraycopy(res, 0, dest_arr,
+                0, resL);
+
+        return dest_arr;
     }
 
-    /**
-     * Method subtract array2 from array1 (using collections) and returns result array
-     *
-     * @param array1 string type array
-     * @param array2 string type array
-     * @return String type result arrayList (collection1 - collection2)
-     */
     ArrayList<String> getSubtractedCollection(String[] array1, String[] array2)
     {
         ArrayList<String> arrayList1 = new ArrayList<>(Arrays.asList(array1));
