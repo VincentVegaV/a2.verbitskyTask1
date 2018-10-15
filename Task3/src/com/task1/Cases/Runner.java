8c2a08b766972596e8d30fd0adbba802644f2c42
VincentVegaV <forgitmail@gmail.com>
2018-10-16 00:14:21 +0300

    Task 3: - Case selection changes

--- Task3/src/com/task1/Cases/Runner.java
+++ Task3/src/com/task1/Cases/Runner.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Runner {
     public static void main(String[] args) {
         SearchCases search = new SearchCases(args[0]);
+        //нужно чтобы пропуск второго значения засчитывался за null
         CaseSelection select = new CaseSelection(args[0], args[1]);
         List<String> initialList;
 
