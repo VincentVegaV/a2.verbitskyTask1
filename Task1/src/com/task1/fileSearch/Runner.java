b87cb1b40a6471e947e0c4f699bf310190bc26bf
VincentVegaV <forgitmail@gmail.com>
2018-10-16 23:57:19 +0300

    Task 3: - Final fixes for print method

--- Task1/src/com/task1/fileSearch/Runner.java
+++ Task1/src/com/task1/fileSearch/Runner.java
@@ -1,11 +1,15 @@
 package com.task1.fileSearch;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 class Runner {
 
     public static void main(String[] args){
-        SearchFile search = new SearchFile(args[0], args[1]);
+        ResourceBundle resourceBundleConfig = ResourceBundle.getBundle("Resources.Config");
+        String path = args[Integer.parseInt(resourceBundleConfig.getString("pathItem"))];
+        String extension = args[Integer.parseInt(resourceBundleConfig.getString("extensionItem"))];
+        SearchFile search = new SearchFile(path, extension);
 
         try{
             PrintFile.printFiles(SortFile.sortListOfFilesByDateDESC(search.returnListOfFileNameAndDate()));
